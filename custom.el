3fb55e28c13160983585c11931c94fbb91a8e167
Pradeep Gowda <pradeep@btbytes.com>
2010-07-28 17:17:13 -0400

    adding mode-aware hook to set key for git stage all.

--- custom.el
+++ custom.el
@@ -8,8 +8,18 @@
 ;;Comment or Uncomment C-c #
 (global-set-key "\C-c#"  'comment-or-uncomment-region)
 
+;; magit
 ;; magit-status C-c ms
 (global-set-key (kbd "C-c ms") 'magit-status)
+
+;; This hook is activated only when the magit-mode is active.
+;; See http://xahlee.org/emacs/keyboard_shortcuts.html
+(add-hook 'magit-mode-hook
+          (lambda ()
+            (define-key magit-mode-map (kbd "C-c sa") 'magit-stage-all)
+            )
+          )
+;;(global-set-key (kbd "C-c sa") 'magit-stage-all)
 
 ;; Line truncation. See http://bit.ly/bzFM05
 ;;(setq truncate-lines t)
