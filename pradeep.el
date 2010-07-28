6a2fc35a54aab35b2fa36006a2714aa98c146127
Pradeep Gowda <pradeep@btbytes.com>
2010-07-28 09:11:27 -0400

    adding "show trailing white spaces".

--- pradeep.el
+++ pradeep.el
@@ -4,3 +4,12 @@
 
 ;;C-c ms magit-status
 (global-set-key (kbd "C-c ms") 'magit-status)
+
+;;Line truncation. See http://bit.ly/bzFM05
+(setq truncate-lines t)
+
+;; ;; (require 'pandoc)
+(add-hook 'markdown-mode-hook 'turn-on-pandoc)
+
+;; show trailing white spaces.
+(setq show-trailing-whitespace 't)
